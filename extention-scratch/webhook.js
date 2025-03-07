7c6066fd5c509c2de33cb992dfe9c3fd15a5f8dc
FodersGames <fodersgames@gmail.com>
2025-03-07 13:51:16 +0100

    Add files via upload

---
+++ extention-scratch/webhook.js
@@ -0,0 +1,50 @@
+class DiscordWebhook {
+    getInfo() {
+      return {
+        id: 'discordwebhook',
+        name: 'Discord Webhook',
+        blocks: [
+          {
+            opcode: 'sendMessage',
+            blockType: 'command',
+            text: 'Envoyer message [MESSAGE] au webhook [URL]',
+            arguments: {
+              MESSAGE: {
+                type: 'string',
+                defaultValue: 'Hello Discord!'
+              },
+              URL: {
+                type: 'string',
+                defaultValue: 'https://discord.com/api/webhooks/...'
+              }
+            }
+          }
+        ]
+      };
+    }
+  
+    sendMessage(args) {
+      const message = args.MESSAGE;
+      const url = args.URL;
+      
+      return fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ content: message }),
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erreur lors de l\'envoi du message');
+        }
+        return 'Message envoyé avec succès';
+      })
+      .catch(error => {
+        return 'Erreur : ' + error.message;
+      });
+    }
+  }
+  
+  Scratch.extensions.register(new DiscordWebhook());
+  
